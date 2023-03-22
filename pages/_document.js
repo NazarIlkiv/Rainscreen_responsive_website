3ba4104c627cfb45a84c0c2090f22ebe4feb6087
Nazar Ilkiv <bruem35@gmail.com>
2023-03-22 16:54:31 +0200

    add google search controle

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,10 @@ export default function Document() {
           href="https://fonts.googleapis.com/css?family=Manrope:regular,500,600,700,800&display=swap"
           rel="stylesheet"
         />
+        <meta
+          name="google-site-verification"
+          content="aBkoz4jlgNGR7T_O_onG_Rr-doKu_qasJ0t7rtN4MB0"
+        />
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
       </Head>
       <body>
