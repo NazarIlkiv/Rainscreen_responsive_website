d720e3373d450bc407bee7e9c0365a6b68ba7ddf
Nazar Ilkiv <bruem35@gmail.com>
2023-03-22 17:47:15 +0200

    Test Schema org in contacts

--- pages/contacts/index.js
+++ pages/contacts/index.js
@@ -1,5 +1,6 @@
 import styles from "../../styles/Contacts.module.scss";
 import stylesRepeat from "@/styles/Repeat.module.scss";
+import { NextSeo } from "next-seo";
 import Head from "next/head";
 
 const contacts = () => {
@@ -15,25 +16,58 @@ const contacts = () => {
         <meta name="robots" content="index,follow" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
+      <NextSeo
+        title="Контакти компанії Rain Screen"
+        description="Контакти компанії Rain Screen - понеділок - п'ятниця з 09:00-18:00, субота з 09:00-15:00."
+        openGraph={{
+          title: "Контакти компанії Rain Screen",
+          description: "Контакти компанії Rain Screen",
+          images: [
+            {
+              url: "https://www.rainscreen.com.ua/images/slider/slide1.jpg",
+              width: 800,
+              height: 600,
+              alt: "Контакти компанії Rain Screen",
+            },
+          ],
+          site_name: "Rain Screen",
+        }}
+        additionalMetaTags={[
+          {
+            name: "robots",
+            content: "index, follow",
+          },
+        ]}
+      />
       <main className={styles.page}>
-        <section className={`${styles.page__contacts} ${styles.contacts}`}>
+        <section
+          itemScope
+          itemType="https://schema.org/Organization"
+          className={`${styles.page__contacts} ${styles.contacts}`}
+        >
           <div className={styles.contacts__container}>
             <h2
               className={`${styles.contacts__title} ${stylesRepeat.title} ${stylesRepeat.title_center}`}
             >
-              <span>Контакти компанії Rain Screen</span>
+              <span itemProp="name">Контакти компанії Rain Screen</span>
             </h2>
             <h3
+              itemProp="telephone"
               className={`${styles.contacts__title} ${stylesRepeat.title} ${stylesRepeat.title_center}`}
             >
               +380936517099
             </h3>
             <div className={styles.contacts__body}>
-              <div className={styles.contacts__text}>
+              <div
+                itemProp="address"
+                itemScope
+                itemType="https://schema.org/PostalAddress"
+                className={styles.contacts__text}
+              >
                 <div>Понеділок - П&#39;ятниця з 09:00-18:00</div>
                 <div>Субота з 09:00-15:00</div>
-                <div>+380936517099</div>
-                <div>rrainscreenfasad@ukr.net</div>
+                <div itemProp="telephone">+380936517099</div>
+                <div itemProp="email">rrainscreenfasad@ukr.net</div>
               </div>
             </div>
           </div>
