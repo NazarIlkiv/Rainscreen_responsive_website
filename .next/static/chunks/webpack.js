a65b5042f10f74a59a0799381bb190ad09d08606
Nazar Ilkiv <bruem35@gmail.com>
2023-03-22 12:59:36 +0200

    first commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1ac87cb6bfbaa912"; }
+/******/ 		__webpack_require__.h = function() { return "c1af1a96eeb5ff5d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
